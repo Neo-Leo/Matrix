000c656e0dfddff52217fbdf538d5260079c6a00
Neo-Leo <sharm398@umn.edu>
2017-01-03 23:47:56 -0600

    Removing global array variable

--- IslandPerimeter.java
+++ IslandPerimeter.java
@@ -4,18 +4,16 @@
  */ 
 
 public class Solution {
-    public void wrapper(int[][] grid, int i, int j, int[] result){
-        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j] != 1) return; 
-        grid[i][j] = 2; 
-        int top = (i==0 || grid[i-1][j] == 0) ? 1 : 0; 
-        int bottom = (i==grid.length-1 || grid[i+1][j] == 0) ? 1 : 0;
-        int left = (j==0 || grid[i][j-1] == 0) ? 1 : 0; 
-        int right = (j == grid[0].length-1 || grid[i][j+1] == 0) ? 1 : 0;
-        result[0] += (left+right+bottom+top);
-        wrapper(grid,i-1,j,result); 
-        wrapper(grid,i+1,j,result);
-        wrapper(grid,i,j-1,result); 
-        wrapper(grid,i,j+1,result);
+    public int wrapper(int[][] grid, int i, int j){
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j] == 0) return 1; 
+        if(grid[i][j] == -1) return 0; 
+        grid[i][j] = -1; 
+        int result = 0;
+        result += wrapper(grid,i-1,j); 
+        result += wrapper(grid,i+1,j);
+        result += wrapper(grid,i,j-1); 
+        result += wrapper(grid,i,j+1);
+        return result; 
     }
     
     public int islandPerimeter(int[][] grid) {
@@ -25,11 +23,10 @@ public class Solution {
         for(int i=0;i<=rows-1;i++){
             for(int j=0;j<=cols-1;j++){
                 if(grid[i][j] == 1){
-                    wrapper(grid, i, j, result); 
-                    break; 
+                    return wrapper(grid, i, j); 
                 }
             }
         }
-        return result[0];
+        return 0;
     }
 }
